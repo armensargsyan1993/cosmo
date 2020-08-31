87f725d210c3089b3bd4919897fa0f5e400551a0
Armen <upravlenciarmen@mail.ru>
2020-08-31 22:35:44 +0400

    1%

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,17 @@
-// import React from 'react';
-// import {Container,Row,Col} from 'react-bootstrap';
+import React from 'react';
+import {Container,Row,Col, Alert} from 'react-bootstrap';
 
-// const Header = () => {
-//     return(
-// <Container className="d-flex">
-//   <Row>
-//     <Col>1 of 3</Col>
-//     <Col xs={6}>2 of 3 (wider)</Col>
-//     <Col>3 of 3</Col>
-//   </Row>
-//   <Row>
-//     <Col>1 of 3</Col>
-//     <Col xs={5}>2 of 3 (wider)</Col>
-//     <Col>3 of 3</Col>
-//   </Row>
-// </Container>
-//     )
-// }
+const Header = () => {
+    return(
+        <header className="header">
+            <Container>
+                <Row>
+                    
+                </Row>
+            </Container>
+        </header>
+    )
+}
 
-// export default Header
+export default Header
 
